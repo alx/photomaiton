b2030ef098df5db92abefb2bf21c85eea089b340
Flo Gales <rataflo@free.fr>
2023-10-26 19:29:46 +0200

    Utils

---
+++ base/boothlauncher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#Launch photo booth python script at startup
+
+cd /
+cd home/minutepapillons/photomaiton/base
+#sudo rm -rf captures
+sudo python -m pip uninstall mastodon-py
+sudo python -m pip install mastodon-py
+sudo python main.py "/media/minutepapillons/BOOTH"
+cd /
