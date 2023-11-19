be13ba0e42936b36a965beb7c46af65503c0493d
Flo Gales <rataflo@free.fr>
2023-11-19 19:18:38 +0100

    feat: shell script to launch booth from crontab

--- base/boothlauncher.sh
+++ base/boothlauncher.sh
@@ -4,7 +4,8 @@
 cd /
 cd home/minutepapillons/photomaiton/base
 #sudo rm -rf captures
-sudo python -m pip uninstall mastodon-py
-sudo python -m pip install mastodon-py
-sudo python main.py "/media/minutepapillons/BOOTH"
+#sudo python -m pip uninstall mastodon-py
+#sudo python -m pip install mastodon-py
+sudo python3 main.py
+#sudo python main.py "/media/minutepapillons/BOOTH"
 cd /
