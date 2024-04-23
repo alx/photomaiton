cfc52b7570e4a2ecab58dadb3652335aae9bf35e
Flo Gales <rataflo@free.fr>
2024-04-23 12:39:28 +0200

    feat printer infos

--- base/printerInfos.py
+++ base/printerInfos.py
@@ -2,6 +2,7 @@
 # Show printer infos
 #
 import cups
+import time
 
 def main():
      # Set up CUPS
@@ -9,15 +10,16 @@ def main():
     printers = conn.getPrinters()
     printer_name = list(printers.keys())[0]
     cups.setUser("pi")
-    printer_attributes = conn.getPrinterAttributes(selected_printer)
-
-    attr = conn.getPrinterAttributes(printer)
+    attr = conn.getPrinterAttributes(printer_name)
     for key, value in attr.items():
-        print(f"{key}: {value}")
+        if key=="marker-message":
+            print(f"{key}: {value}")
 
     return 1
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
+        time.sleep(300)
 
